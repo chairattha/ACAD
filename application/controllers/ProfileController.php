5fc61d54ec1b94cd161456dc348c50dd2fbf669d
artitbook43@gmail.com <artitbook43@gmail.com>
2023-10-12 03:57:39 -0500

    [+] add input more like personel insert form

--- application/controllers/ProfileController.php
+++ application/controllers/ProfileController.php
@@ -23,6 +23,7 @@ class ProfileController extends CI_Controller
     public function profileIndex()
     {
         $data["row"] = $this->PersonnelModel->get_personnel_by_user_id($this->session->userdata("userId"));
+        $data["type"] = $this->PersonnelModel->get_personnel_type($this->session->userdata("userSchoolId"));
         $this->load->view("layout/header");
         $this->load->view("profile/profileIndex", $data);
         $this->load->view("layout/footer");
@@ -30,13 +31,31 @@ class ProfileController extends CI_Controller
 
     public function profile_update()
     {
-        $data = array(
-            'titlename' => $_POST["inTitleName"],
-            'firstname' => $_POST["inFirstName"],
-            'lastname' => $_POST["inLastName"],
-            'profile_image' => $_POST["inPersonnelLogo64"]
+        $arr = array(
+            "school_id" => $this->session->userdata("userSchoolId"),
+            "personneltypeid" => $this->input->post("inType"),
+            "profile_image" => $this->input->post("inPersonnelLogo64"),
+            "titlename" => $this->input->post("inTitleName"),
+            "firstname" => $this->input->post("inFirstName"),
+            "lastname" => $this->input->post("inLastName"),
+            "nickname" => $this->input->post("inNickName"),
+            "idcard" => $this->input->post("inIdCard"),
+            "birthdate" => $this->input->post("inBirthDate"),
+            "gender" => $this->input->post("inGenDer"),
+            "bloodtype" => $this->input->post("inBloodType"),
+            "phonenumber" => $this->input->post("inPhoneNumber"),
+            "religion" => $this->input->post("inReligion"),
+            "ethnicity" => $this->input->post("inEthnicity"),
+            "nationality" => $this->input->post("inNationality")
         );
-        $this->PersonnelModel->update_personnel($_POST["inPersonnelId"], $data);
+       echo  $this->PersonnelModel->update_personnel($arr, $this->input->post("inPer"));
+        // $data = array(
+        //     'titlename' => $_POST["inTitleName"],
+        //     'firstname' => $_POST["inFirstName"],
+        //     'lastname' => $_POST["inLastName"],
+        //     'profile_image' => $_POST["inPersonnelLogo64"]
+        // );
+        // $this->PersonnelModel->update_personnel($_POST["inPersonnelId"], $data);
 
         $Personnel = $this->PersonnelModel->get_personnel_by_user_id($this->session->userdata("userId"));
         $this->session->set_userdata('userName', $Personnel["personnel_fullname"]);
